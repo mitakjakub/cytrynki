d7725d6e3babf65807843d2a0a5a04bbeec4f6b6
Jakub Mitak <jakub@mitak.pl>
2021-10-26 21:02:21 +0200

    zmiany

--- build/database.sh
+++ build/database.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
     data=`date +'%Y-%m-%d'`
-    url=`curl -d "data=$data&submit=Zmie%C5%84+dat%C4%99" -H "Content-Type: application/x-www-form-urlencoded" -X POST https://zlobki.waw.pl/nasze-placowki/statystyki/ | grep zlobek-nr-69 | cut -d '"' -f2`
-    ilosc=`curl -s $url | grep -A1 'Dzieci obecnych' | grep -m 1 [0-9] | tr -d '<>td' | awk '{ print $1 }'`
-
-if [ ! $ilosc ]; then
-    ilosc='0';
+    url=`curl -s -d "data=$data&submit=Zmie%C5%84+dat%C4%99" -H "Content-Type: application/x-www-form-urlencoded" -X POST https://zlobki.waw.pl/nasze-placowki/statystyki/ | grep zlobek-nr-69 | cut -d '"' -f2`
+    curl -s $url -o /tmp/file.html
+    array=()
+    for i in "obecnych" "zapisanych" "opiekunów"; do
+        array+=(`cat /tmp/file.html | grep -A1 $i | grep -m 1 [0-9] | tr -d '</td>' | awk '{ print $1 }'`)
+    done;
+if [ ! ${array[0]} ]; then
+    array[0]='0'
+    array[1]='0'
+    array[2]='0'
 fi
-sqlite3 /cytrynkidb/cytrynki.db "insert into cytrynki(data, ilosc) values(\"$data\", $ilosc)"
+    x=`echo ${array[0]} | sed 's/[^0-9]*//g'`
+    y=`echo ${array[1]} | sed 's/[^0-9]*//g'`
+    z=`echo "${array[2]}" | sed 's/[^0-9]*//g' `
+    echo "$w|$x|$y|$z" >> bulk.txt 
+
+> /tmp/file.html
+sqlite3 /cytrynkidb/cytrynki.db "insert into cytrynki(data, ilosc, iloscz, ilosco) values(\"$data\", \"$x\", \"$z\", \"$y\")"
--- build/database_bulk.sh
+++ build/database_bulk.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 d='20210901'
-until [[ $d -eq `date +'%Y%m%d'` ]]; 
-do 
-    ddata=`date --date "$d" +'%Y-%m-%d'`
-    url=`curl -d "data=$ddata&submit=Zmie%C5%84+dat%C4%99" -H "Content-Type: application/x-www-form-urlencoded" -X POST https://zlobki.waw.pl/nasze-placowki/statystyki/ | grep zlobek-nr-69 | cut -d '"' -f2`
-    ilosc=`curl -s $url | grep -A1 'Dzieci obecnych' | grep -m 1 [0-9] | tr -d '<>td' | awk '{ print $1 }'`
-
-if [ ! $ilosc ]; then
-    ilosc='0';
-fi
+until [[ $d -eq `date +'%Y%m%d'` ]]; do 
+    data=`date --date "$d" +'%Y-%m-%d'`
+    url=`curl -s -d "data=$data&submit=Zmie%C5%84+dat%C4%99" -H "Content-Type: application/x-www-form-urlencoded" -X POST https://zlobki.waw.pl/nasze-placowki/statystyki/ | grep zlobek-nr-69 | cut -d '"' -f2`
+    curl -s $url -o /tmp/file.html
+    array=()
+    for i in "obecnych" "zapisanych" "opiekunów"; do
+    array+=(`cat /tmp/file.html | grep -A1 $i | grep -m 1 [0-9] | tr -d '</td>' | awk '{ print $1 }'`)
+    done;
+    if [ ! ${array[0]} ]; then
+        array[0]='0'
+        array[1]='0'
+        array[2]='0'
+    fi
+    > /tmp/file.html
     d=$(date --date "$d + 1 day" +'%Y%m%d');
-    echo "$ddata|$ilosc" >> bulk.txt    
-sqlite3 /cytrynkidb/cytrynki.db "insert into cytrynki(data, ilosc) values(\"$data\", $ilosc)"
+    w="$data"
+    x=`echo ${array[0]} | sed 's/[^0-9]*//g'`
+    y=`echo ${array[1]} | sed 's/[^0-9]*//g'`
+    z=`echo "${array[2]}" | sed 's/[^0-9]*//g' `
+    echo "$w|$x|$y|$z" >> bulk.txt    
 done
-
 
